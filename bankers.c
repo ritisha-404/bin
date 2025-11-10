5110a65701813bddfed0231369999ad1a4dc589e
Ritisha <145014430+ritisha-404@users.noreply.github.com>
2025-11-10 13:10:30 +0530

    Refactor Bankers algorithm for dynamic array sizes

--- bankers.c
+++ bankers.c
@@ -17,12 +17,12 @@ void BankerssafeSequence(int r, int c, bool pFinish[], int need[][c], int avl[],
     int processCount = 0;
     int k = 0;
     bool temp, process = false;
-    int safeorder[10]; // Stores safe execution order (up to 10 processes)
+    int safeorder[r]; // Dynamic array size
 
     while (processCount < r) {
         process = false;
         for (int i = 0; i < r; i++) {
-            if (pFinish[i]) continue; // Skip finished processes
+            if (pFinish[i]) continue;
             temp = true;
             for (int j = 0; j < c; j++) {
                 if (need[i][j] > avl[j]) {
@@ -32,9 +32,8 @@ void BankerssafeSequence(int r, int c, bool pFinish[], int need[][c], int avl[],
             }
             if (!temp) continue;
 
-            // Process can safely execute
             for (int j = 0; j < c; j++)
-                avl[j] += alloc[i][j]; // Release resources
+                avl[j] += alloc[i][j];
 
             pFinish[i] = true;
             processCount++;
@@ -71,12 +70,11 @@ void ResourcesRequest(int r, int c, int avl[], bool pFinish[], int need[][c], in
         printf("\nFor which process?\n>> ");
         scanf("%d", &p);
 
-        int addResources[10];
+        int addResources[c];
         printf("Enter amount of each resource requested:\n>> ");
         for (int i = 0; i < c; i++)
             scanf("%d", &addResources[i]);
 
-        // Validate request
         for (int i = 0; i < c; i++) {
             if (addResources[i] > need[p][i]) {
                 printf("\nError: Request exceeds process need.\n");
@@ -88,7 +86,6 @@ void ResourcesRequest(int r, int c, int avl[], bool pFinish[], int need[][c], in
             }
         }
 
-        // Grant the request
         for (int i = 0; i < c; i++) {
             alloc[p][i] += addResources[i];
             avl[i] -= addResources[i];
@@ -100,7 +97,6 @@ void ResourcesRequest(int r, int c, int avl[], bool pFinish[], int need[][c], in
     }
 }
 
-// Main function
 int main() {
     char again;
     do {
@@ -127,11 +123,9 @@ int main() {
 
         for (int i = 0; i < c; i++)
             avlCopy[i] = avl[i];
-
         for (int i = 0; i < r; i++)
             pFinish[i] = false;
 
-        // Calculate Need = Max - Allocation
         for (int i = 0; i < r; i++)
             for (int j = 0; j < c; j++)
                 need[i][j] = max[i][j] - alloc[i][j];
@@ -143,16 +137,13 @@ int main() {
             printf("\n");
         }
 
-        // Run Safety Algorithm
         BankerssafeSequence(r, c, pFinish, need, avl, alloc);
 
-        // Reset available and finish arrays for next check
         for (int i = 0; i < c; i++)
             avl[i] = avlCopy[i];
         for (int i = 0; i < r; i++)
             pFinish[i] = false;
 
-        // Handle optional request
         ResourcesRequest(r, c, avl, pFinish, need, alloc);
 
         clearInputBuffer();
@@ -162,7 +153,5 @@ int main() {
     } while (again == 'y' || again == 'Y');
 
     printf("\nProgram terminated.\n");
-    return 0;}
-
-//gcc banker.c -o bankers
+    return 0;
 }
